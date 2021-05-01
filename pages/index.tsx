60b76a52747548159a188b3f4aa8f1e4b29e93d1
Mathias Storm <mathias@storm.lc>
2021-05-01 21:36:31 +0200

    add headline

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
+import Head from 'next/head';
+import React from "react";
 import styles from "../styles/Home.module.css";
-
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <main>blog</main>
+      <Head>
+        <title>CapturedContext</title>
+        <meta name="description" content="Interesting articles about Technology featuring React, GraphQL and DevOps." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>
+          <span className={styles.headline}>CapturedContext</span>
+          <span className={styles.domain}>.dev</span>
+        </h1>
+      </main>
     </div>
   )
 }
